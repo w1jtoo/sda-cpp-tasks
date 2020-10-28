3a0685ead755f3b415ca545a6449d7588980afcf
w1jtoo <wanadooht@gmail.com>
2020-10-28 23:26:02 +0500

    memory performance solution

--- week3/memory-performance/build.sh
+++ week3/memory-performance/build.sh
@@ -1,1 +1,1 @@
-g++ -Wall -Werror -pedantic -o main.elf main.cpp
+g++ -O2 -Wall -Werror -pedantic -o main.elf main.cpp
--- week3/memory-performance/main.cpp
+++ week3/memory-performance/main.cpp
@@ -10,10 +10,68 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
+ *         Author:  w1jtoo
  *   Organization:  
  *
  * =====================================================================================
  */
+
 #include <stdlib.h>
+#include <iostream>
+#define TESTS_COUNT 1000000
+
+static int sum = 0;
+
+void stack_array_allocation(void);
+void heap_array_allocation(void);
+void print_sum(int array[10]);
+
+/* ===== RESULTS =====
+ * ** | stack | heap  
+ * ---+-------+-------
+ * o2 | 3392  | 20084 
+ * ---+-------+-------
+ * o3 |  278  | 15188 
+ */
+
+int main(void) { 
+	auto t = clock();
+	for (int j=0; j < 1000; ++j) {
+		for (int i=0; i < TESTS_COUNT; ++i){
+			stack_array_allocation();
+		}
+	}
+	auto t1 = (clock() - t) / 1000;
+	printf("First sum %i\n", sum);
+
+	t = clock();
+	for (int j = 0; j < 1000; ++j) {
+		for (int i=0; i < TESTS_COUNT; ++i){
+			heap_array_allocation();
+		}
+	}
+	auto t2 = (clock() - t) / 1000;
+	printf("First sum %i\n", sum);
+
+	printf("In took %ld ms to allocate array of 10 int on stack %i times\n", t1, TESTS_COUNT);
+	printf("In took %ld ms to allocate array of 10 int on heap %i times\n", t2, TESTS_COUNT);
+}
+
+void stack_array_allocation(void) {
+	int a[10] = { };
+	print_sum(a);
+}
+
+void heap_array_allocation(void) {
+	int* a = new int[10]();
+	print_sum(a);
+	delete a;
+}
+
+void print_sum(int array[10]) {
+	sum = 0;
+	for(int i = 0; i < 10; ++i) { 
+		sum += array[i];
+	}
+}
 
