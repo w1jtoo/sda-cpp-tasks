0f375ba283d58c2d0ce8cc7d060e7b03140b05ae
w1jtoo <43470708+w1jtoo@users.noreply.github.com>
2020-10-29 11:39:19 +0500

    Update build.sh

--- week3/dll-pingpong/build.sh
+++ week3/dll-pingpong/build.sh
@@ -1,1 +1,9 @@
-g++ main.cpp -o main ping.cpp pong.cpp
+g++  main.cpp -o bin/main.o
+
+g++ -fPIC -o ping.so -fPIC bin/ping.cpp
+g++ -fPIC -o pong.so -fPIC bin/pong.cpp
+g++ -shared   bin/ping.o bin/pong.o -o bin/pingpong.so
+
+g++ bin/main.o ./libcalc.so -lm
+
+
